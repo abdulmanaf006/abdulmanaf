a6b34341061c7990a75c270ffe8ffad216db7352
Abdulmanaf <abdulmanaf006@gmail.com>
2025-09-08 14:41:32 +0400

    Add files via upload

---
+++ processForm.php
@@ -0,0 +1,20 @@
+<?php
+	header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Thank you for contact us. As early as possible  we will contact you !'
+	);
+
+    $name = @trim(stripslashes($_POST['name'])); 
+    $email = @trim(stripslashes($_POST['email'])); 
+	$subject = "CONTACT DETAILS";    
+    $message = @trim(stripslashes($_POST['message'])); 
+
+    $email_from = $email;
+    $email_to = 'abdulmanaf006@gmail.com';
+    $body =  'Subject: ' . $subject . "\n\n\n" . 'Full Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" .  'Message: ' . $message;
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+    echo json_encode($status);
+    die;
+
+
